170ee12ec9a43f6a3df42afff41eb77badbd6d49
gockxml <hustgock@gmail.com>
2018-09-28 16:37:55 +0800

    update for 1.1.2

--- src/changelog.vue
+++ src/changelog.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="changelog ui card cc-item">
     <div class="cc-item-header content">
-      <div class="header">1.1.1 更新日志</div>
+      <div class="header">{{ version }} 更新日志</div>
     </div>
     <div class="cc-item-content content cc-content">
       <div class="cc-text">
         <dl>
-          <dd>增加修改本地数据目录的选项</dd>
-          <dd>修复 Word 和 Excel 的误触发问题</dd>
-          <dd>忽略来自常见密码管理软件的内容</dd>
-          <dd>增加开启测试版更新的选项</dd>
+          <dd>增加 <kbd>多选</kbd> 功能，可以对多个项目进行删除</dd>
+          <dd>增加 <kbd>合并</kbd> 功能，多选之后可以合并文本或文件类型</dd>
+          <dd>增加 <kbd>文本编辑</kbd> 功能，可以对文本项目进行修改</dd>
+          <dd>修正托盘图标的 <kbd>关于</kbd> 功能</dd>
           <dd>修复其他 bug</dd>
         </dl>
       </div>
@@ -26,8 +26,10 @@ export default {
       this.$emit("dismiss")
     }
   },
-  mounted(){
-    
+  computed: {
+    version() {
+      return cc.getVersion()
+    }
   }
 }
 </script>
